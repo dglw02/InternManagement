4cfea1b6c09281b0e01c0b8c420e952bbaf3cc6f
dglw02 <luuhaiduong100@gmail.com>
2023-04-07 10:24:28 +0700

    thêm phần tạo công việc cho thực tập sinh

--- src/main/java/com/example/internmanagement/entity/Project.java
+++ src/main/java/com/example/internmanagement/entity/Project.java
@@ -1,24 +1,28 @@
 package com.example.internmanagement.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Setter
-@Getter
-@NoArgsConstructor
+@Data
 @AllArgsConstructor
-@Table(name="tbl_class")
-public class Class {
+@NoArgsConstructor
+@Table(name = "tbl_project")
+public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
-    private String name;
+    private String projectName;
+
+    @Column
+    private String projectDescription;
+
+
+
 
 }
---
+++ src/main/java/com/example/internmanagement/service/ProjectService.java
@@ -0,0 +1,39 @@
+package com.example.internmanagement.service;
+
+import com.example.internmanagement.entity.Project;
+import com.example.internmanagement.exception.ErrorException;
+import com.example.internmanagement.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public Project saveProject(@Valid Project project) {
+        return projectRepository.save(project);
+    }
+
+    public void deleteProjectById(Long id) {
+        if (id == 0) {
+            throw new ErrorException("You need to provide ID of project to be deleted. ID can not be 0.");
+        }
+        Optional<Project> checkIfProjecttWithIdExist = projectRepository.findById(id);
+        if (checkIfProjecttWithIdExist.isEmpty()) {
+            throw new ErrorException(
+                    "Student can not be deleted because project with id: " + id + " does not exist.");
+        }
+        projectRepository.deleteById(id);
+    }
+}
